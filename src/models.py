dc54fb7faf0e1d4fc81ed24f16a870f90f5e474a
iluna007 <146373472+iluna007@users.noreply.github.com>
2024-04-23 02:50:25 +0000

    inicio

--- src/models.py
+++ src/models.py
@@ -17,3 +17,37 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+    
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    last_name = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<People %r>' % self.name
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "last_name": self.last_name,
+            
+            # do not serialize the password, its a security breach
+        }  
+
+class Planets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    size = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<Planets %r>' % self.name
+        
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "size": self.size,
+            
+            # do not serialize the password, its a security breach
+        }
